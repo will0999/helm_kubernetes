626d38b0bfa8e75375cc4a3569d46303d16d1b87
will <56336577+will0999@users.noreply.github.com>
2023-07-03 09:27:08 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Update dependency
-helm dependency update
+# helm dependency update
 
 # Deploy Redis
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install redis bitnami/redis -f Redis/config/values.yaml
 
 # Deploy Guestbook com Redis
 helm install guestbook Guestbook -f Guestbook/config/values.yaml
+
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm install redis bitnami/redis -values Redis/config/values.yaml -n guestbook
+#helm install guestbook Guestbook -n guestbook
